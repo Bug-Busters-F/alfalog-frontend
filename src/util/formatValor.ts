e7560efe96ee5eeb56ec78a6c5a72ccc8cf4ed9c
Diego <diegocastilholorenceto@gmail.com>
2025-05-22 21:35:14 -0300

    feat: add bilhoes to formatValor

--- src/util/formatValor.ts
+++ src/util/formatValor.ts
@@ -1,5 +1,11 @@
 export function formatarValor(valor: number): string {
-  if (valor >= 1_000_000) {
+  if (valor >= 1_000_000_000) {
+    const bilhoes = valor / 1_000_000_000;
+    return `R$ ${bilhoes.toLocaleString("pt-BR", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })} bi`;
+  } else if (valor >= 1_000_000) {
     const milhoes = valor / 1_000_000;
     return `R$ ${milhoes.toLocaleString("pt-BR", {
       minimumFractionDigits: 2,
